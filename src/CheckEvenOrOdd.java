930304f9ac1bd2e094685c04c587b446ef0f0304
madhumaddela <madhu21897@gmail.com>
2020-02-21 22:57:37 +0530

    Added sysout

--- src/CheckEvenOrOdd.java
+++ src/CheckEvenOrOdd.java
@@ -5,6 +5,7 @@ public class CheckEvenOrOdd {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Enter a Number to check even or odd1");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter a Number to check even or odd");
 		int number = scan.nextInt();
