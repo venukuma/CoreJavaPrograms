f771645bba627d68bbd66faa6260301c4589af2f
= <=>
2020-02-21 21:14:42 +0530

    First Commit

---
+++ src/CheckEvenOrOdd.java
@@ -0,0 +1,14 @@
+public class CheckEvenOrOdd {
+
+	public static void main(String[] args) {
+		
+		int i =100;
+		if(i%2==0) {
+			System.out.println("given number is even");
+		}
+		else
+		System.out.println("Given Number is odd");
+
+	}
+
+}
